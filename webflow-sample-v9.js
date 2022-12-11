7cb38c62f4de895de2698859a32a3f5c0db6fb1f
hafizhizers <34477194+hafizhizers@users.noreply.github.com>
2022-12-11 15:12:46 +0800

    Update webflow-sample-v9.js

--- webflow-sample-v9.js
+++ webflow-sample-v9.js
@@ -1,113 +1,114 @@
 window.onload = function() {
-	const token = getSavedData("authToken");
-			if(token == null){
-      	alert('You are not logged in. Please log in and try again');
-        location.href="https://atfals-site.webflow.io"
-			}
-      else {
-      	getProductList(1)
-      }
+    const token = getSavedData("authToken");
+    if (token == null) {
+        alert('You are not logged in. Please log in and try again');
+        location.href = "https://atfals-site.webflow.io"
+    } else {
+        getProductList(1)
+    }
 }
 
 function pageList(totalRecords, recordsPerPage) {
-  // Calculate the total number of pages
-  const totalPages = Math.ceil(totalRecords / recordsPerPage);
+    // Calculate the total number of pages
+    const totalPages = Math.ceil(totalRecords / recordsPerPage);
 
-  // Create an array of page numbers
-  const pageNumbers = Array.from(Array(totalPages).keys());
-  return pageNumbers;
+    // Create an array of page numbers
+    const pageNumbers = Array.from(Array(totalPages).keys());
+    return pageNumbers;
 }
 
-function getProductList(page = 1) { 
+function getProductList(page = 1) {
 
     const url = "https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/order?";
-    const pagination = { "page": page };
+    const pagination = {
+        "page": page
+    };
     const fullUrl = url + "pagination=" + encodeURIComponent(JSON.stringify(pagination));
     const token = getSavedData("authToken");
-	
+
 
     fetchAPI('https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/order', 'GET', token)
-     .then(data => {
-      console.log('data',data);
-      populateToShipmentTable(data.items);
-      shipmentTablePaginationList(data)
-    })
-    .catch(error => {
-    // Handle any errors here
-    });
-     
+        .then(data => {
+            console.log('data', data);
+            populateToShipmentTable(data.items);
+            shipmentTablePaginationList(data)
+        })
+        .catch(error => {
+            // Handle any errors here
+        });
+
 }
 
 
-function populateToShipmentTable(data){
+function populateToShipmentTable(data) {
 
-		const parentTable = document.getElementById('table-buy-for-me-list');
+    const parentTable = document.getElementById('table-buy-for-me-list');
     const style = document.getElementById('table-buy-for-me-row')
     const emptyDiv = [];
- 
- 		data.forEach(data => { 
-      const card = style.cloneNode(true)
-      const divs = card.getElementsByTagName('div') 
-      const orderId = divs[1]
-      orderId.textContent = data.id;
-      const destination = divs[2]
-      destination.textContent = `${data.warehouse_data.short_code} - ${data.destination_data.short_code}`;
-      const dateTime = divs[4]
-      dateTime.textContent = data.created_at;
-      const price = divs[6]
-      price.textContent = data.price;
-      const weight = divs[8]
-      weight.textContent = data.weight;
-      const status = divs[11]
-      status.textContent = data.status_data.type;
-   
-      emptyDiv.push(card)
+
+    data.forEach(data => {
+        const card = style.cloneNode(true)
+        const divs = card.getElementsByTagName('div')
+        const orderId = divs[1]
+        orderId.textContent = data.id;
+        const destination = divs[2]
+        destination.textContent = `${data.warehouse_data.short_code} - ${data.destination_data.short_code}`;
+        const dateTime = divs[4]
+        dateTime.textContent = data.created_at;
+        const price = divs[6]
+        price.textContent = data.price;
+        const weight = divs[8]
+        weight.textContent = data.weight;
+        const status = divs[11]
+        status.textContent = data.status_data.type;
+
+        emptyDiv.push(card)
     })
 
-   // Remove all old child nodes 
-  while (parentTable.firstChild) {
-    parentTable.removeChild(parentTable.firstChild);
-  }
-  
-   emptyDiv.forEach(item => {
-   parentTable.appendChild(item);
-  });
+    // Remove all old child nodes 
+    while (parentTable.firstChild) {
+        parentTable.removeChild(parentTable.firstChild);
+    }
+
+    emptyDiv.forEach(item => {
+        parentTable.appendChild(item);
+    });
 }
 
 function shipmentTablePaginationList(data) {
-  const paginationData = pageList(data.itemsTotal, data.itemsReceived);
-  const paginationList = document.getElementById('table-buy-for-me-pagination');
-  const paginationColumn = document.getElementById('table-buy-for-me-pagination-column')
-  const emptyDiv = [];
-  
-  paginationData.forEach(number => {
-     const clonePaginationColumn = paginationColumn.cloneNode(false)
-     clonePaginationColumn.innerHTML = number + 1;
-     clonePaginationColumn.addEventListener('click', function(event) {
-        // Get the current page number from the pagination element
-        const currentPage = parseInt(event.target.textContent);
-        getAllShipmentList(currentPage)
-      });
-    //emptyDiv.appendChild(clonePaginationColumn);
-    emptyDiv.push(clonePaginationColumn)
-  });
-  
-   // Remove all old child nodes 
-  while (paginationList.firstChild) {
-    paginationList.removeChild(paginationList.firstChild);
-  }
-  
-  emptyDiv.forEach(item => {
-   paginationList.appendChild(item);
-  });
- 
+    const paginationData = pageList(data.itemsTotal, data.itemsReceived);
+    const paginationList = document.getElementById('table-buy-for-me-pagination');
+    const paginationColumn = document.getElementById('table-buy-for-me-pagination-column')
+    const emptyDiv = [];
+
+    paginationData.forEach(number => {
+        const clonePaginationColumn = paginationColumn.cloneNode(false)
+        clonePaginationColumn.innerHTML = number + 1;
+        clonePaginationColumn.addEventListener('click', function(event) {
+            // Get the current page number from the pagination element
+            const currentPage = parseInt(event.target.textContent);
+            getProductList(currentPage)
+        });
+        //emptyDiv.appendChild(clonePaginationColumn);
+        emptyDiv.push(clonePaginationColumn)
+    });
+
+    // Remove all old child nodes 
+    while (paginationList.firstChild) {
+        paginationList.removeChild(paginationList.firstChild);
+    }
+
+    emptyDiv.forEach(item => {
+        paginationList.appendChild(item);
+    });
+
 }
 
 function clearSession(event) {
-  	event.preventDefault();
+    event.preventDefault();
     window.localStorage.removeItem('authToken');
     // window.location.reload();
- 		location.href = "https://atfals-site.webflow.io";
+    location.href = "https://atfals-site.webflow.io";
 }
 
 const logout = document.getElementById('navlink-log-out');
@@ -122,13 +123,13 @@ function populateToSelectWarehouse(data) {
 
     dropdown.add(defaultOption);
     dropdown.selectedIndex = 0;
-    
+
     let option;
     for (let i = 0; i < data.length; i++) {
-      option = document.createElement('option');
-      option.text = data[i].warehouse_name;
-      option.value = data[i].id;
-      dropdown.add(option);
+        option = document.createElement('option');
+        option.text = data[i].warehouse_name;
+        option.value = data[i].id;
+        dropdown.add(option);
     }
 }
 
@@ -141,62 +142,61 @@ function populateToSelectExpressCompany(data) {
 
     dropdown.add(defaultOption);
     dropdown.selectedIndex = 0;
-    
+
     let option;
     for (let i = 0; i < data.length; i++) {
-      option = document.createElement('option');
-      option.text = data[i].company_name;
-      option.value = data[i].id;
-      dropdown.add(option);
+        option = document.createElement('option');
+        option.text = data[i].company_name;
+        option.value = data[i].id;
+        dropdown.add(option);
     }
 }
 
-function getWarehouseList() { 
+function getWarehouseList() {
     fetch("https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/warehouse", {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        //'Authorization': 'Bearer ' + authWarehouse
-        }
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                //'Authorization': 'Bearer ' + authWarehouse
+            }
         })
 
         .then(res => res.json())
-				.then(json => {
-        	const xanoResponse = json;
-          populateToSelectWarehouse(xanoResponse);
-         })
+        .then(json => {
+            const xanoResponse = json;
+            populateToSelectWarehouse(xanoResponse);
+        })
 }
 
-function getExpressCompanyList() { 
-	
+function getExpressCompanyList() {
+
     fetch("https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/express_company", {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        //'Authorization': 'Bearer ' + authWarehouse
-        }
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                //'Authorization': 'Bearer ' + authWarehouse
+            }
         })
 
         .then(res => res.json())
-				.then(json => {
-        	const xanoResponse = json;
-          populateToSelectExpressCompany(xanoResponse);
-         })
+        .then(json => {
+            const xanoResponse = json;
+            populateToSelectExpressCompany(xanoResponse);
+        })
 }
 
-function handleApiRequest(){
+function handleApiRequest() {
     event.preventDefault();
-		getWarehouseList();
-		getExpressCompanyList();
+    getWarehouseList();
+    getExpressCompanyList();
 }
 
 const createShipmentBtn = document.getElementById('button-create-shipment');
 createShipmentBtn.addEventListener('click', handleApiRequest);
 
 function postShipmentData() {
-	const xano_input = 
-    {
-    		warehouse_id: document.getElementById('Contact-Modal-6-Select-5-Warehouse').value,
+    const xano_input = {
+        warehouse_id: document.getElementById('Contact-Modal-6-Select-5-Warehouse').value,
         product_name: document.getElementById('Contact-Modal-6-Product-Name').value,
         express_company_id: document.getElementById('Contact-Modal-6-Select-3-express-company').value,
         express_number: document.getElementById('Contact-Modal-6-Express-Number').value,
@@ -205,35 +205,29 @@ function postShipmentData() {
         remarks: document.getElementById('Contact-Modal-6-Product-Remark').value
         // You can add other inputs here if you want
     };
-    
+
     fetch("https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/order", {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(xano_input),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(xano_input),
         })
-				// handle response
+        // handle response
         .then(res => res.json())
         .then(json => {
-        	const xanoResponse = json;
-        	// console.log(xanoResponse);
-          if(xanoResponse.message){
-          	alert(xanoResponse.message);   
-          }
-        }
-        );
+            const xanoResponse = json;
+            // console.log(xanoResponse);
+            if (xanoResponse.message) {
+                alert(xanoResponse.message);
+            }
+        });
 }
 
 function shipmentFormSubmit(event) {
-  	event.preventDefault();
-		postShipmentData()
+    event.preventDefault();
+    postShipmentData()
 }
 
 const shipmentForm = document.getElementById('wf-form-Contact-Modal-6-Form');
 shipmentForm.addEventListener('submit', shipmentFormSubmit, true);
-
-
-
-
-
