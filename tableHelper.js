888e77ae91fed334b5b22f55aaafe1399c89b4fa
hafizhizers <34477194+hafizhizers@users.noreply.github.com>
2022-12-11 15:19:49 +0800

    Create tableHelper.js

---
+++ tableHelper.js
@@ -0,0 +1,8 @@
+function pageList(totalRecords, recordsPerPage) {
+    // Calculate the total number of pages
+    const totalPages = Math.ceil(totalRecords / recordsPerPage);
+
+    // Create an array of page numbers
+    const pageNumbers = Array.from(Array(totalPages).keys());
+    return pageNumbers;
+}
