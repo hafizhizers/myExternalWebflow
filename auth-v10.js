34ed8aa530f2795fda0f3e1c49b8dfd24e338f39
hafizhizers <34477194+hafizhizers@users.noreply.github.com>
2022-12-15 16:12:38 +0800

    Update auth-v10.js

--- auth-v10.js
+++ auth-v10.js
@@ -29,38 +29,6 @@ function signUpformSubmit(event) {
 const signUpform = document.getElementById('wf-form-Sign-up-Form-9');
 signUpform.addEventListener('submit', signUpformSubmit, true);
 
-// function xanoSignUp() {
-// 		const xano_input = 
-//     {
-//         Customer_Name: document.getElementById('Form-9-Name-2').value,
-//         Customer_Email: document.getElementById('Form-9-Email-2').value,
-//         Customer_Password: document.getElementById('Form-9-Password-3').value
-//         // You can add other inputs here if you want
-//     };
-//     fetch("https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/auth/signup", {
-//         method: 'POST',
-//         headers: {
-//         'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(xano_input),
-//         })
-// 				// handle response
-//         .then(res => res.json())
-//         .then(json => {
-//         	const xanoResponse = json;
-//         	const hasKey = Object.keys(xanoResponse).includes("authToken");
-//           if (hasKey === false) {
-//           	alert("User already exists.")}
-//             else {
-//               // const authToken = xanoResponse.authToken;
-//               // localStorage.setItem('authToken', authToken);
-//               alert("Your account was created successfully. Please login to access the website");
-//               //location.href = "[WHERE TO REDIRECT THE USER AFTER SIGNUP]";
-//               // console.log('authToken',authToken)
-//               };}
-//         );
-// }
-
 function postSignUp() {
   
     // Generate a unique ID for a new user
@@ -118,39 +86,6 @@ function postLogin() {
   
 }
 
-// function xanoLogin() {
-// 	const xano_input = 
-//     {
-//         Customer_Email: document.getElementById('Log-in-9-Email-2').value,
-//         Customer_Password: document.getElementById('Log-in-9-Password-2').value,
-//         // You can add other inputs here if you want
-//     };
-    
-//     fetch("https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/auth/login", {
-//         method: 'POST',
-//         headers: {
-//         'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(xano_input),
-//         })
-// 				// handle response
-//         .then(res => res.json())
-//         .then(json => {
-//         	const xanoResponse = json;
-//         	// console.log(xanoResponse);
-//         	const hasKey = Object.keys(xanoResponse).includes("authToken");
-//           // console.log(hasKey);
-//           if (hasKey === false) {
-//           	alert("Invalid email or password.")}
-//             else {
-//             	// alert("We have an authToken!");
-//               const authToken = xanoResponse.authToken;
-//               //localStorage.setItem('authToken', authToken);
-//               saveData('authToken', authToken);
-//               location.href = "https://atfals-site.webflow.io/singleitem";
-//               };}
-//         );
-// }
 
 function loginformSubmit(event) {
   	event.preventDefault();
