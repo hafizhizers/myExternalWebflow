b6bfa404def5ef1eb7109965f10b22055998e26a
hafizhizers <34477194+hafizhizers@users.noreply.github.com>
2022-12-17 15:24:31 +0800

    Update and rename tableHelper-v3.js to tableHelper-v4.js

--- tableHelper-v4.js
+++ tableHelper-v4.js
@@ -1,13 +1,3 @@
-function getPageList(totalRecords, recordsPerPage) {
-    // Calculate the total number of pages
-    const totalPages = Math.ceil(totalRecords / recordsPerPage);
-
-    // Create an array of page numbers
-    const pageNumbers = Array.from(Array(totalPages).keys());
-    return pageNumbers;
-}
-
-
 function displayPaginationList(data, listElementId, listElementColumnId, functionNameEveryOnClick) {
     
     const total = data.pageTotal;
