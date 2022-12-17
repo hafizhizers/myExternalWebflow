2cc3600bd0c841488196c9f583e5b998bb6ad43d
hafizhizers <34477194+hafizhizers@users.noreply.github.com>
2022-12-17 15:27:18 +0800

    Update eWallet-v38.js

--- eWallet-v38.js
+++ eWallet-v38.js
@@ -131,4 +131,83 @@ function postEwalletData(){
 }
 
 
+let curPage = 1;
+let pageTotal = null;
+let nextPage = null;
+let prevPage = null;
+
+function getTransactionData(page) {
+  const url = "https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/top_up_by_user?";
+  const pagination = { "page": page };
+  const userData = getSavedData("userData");
+  const fullUrl = url + "pagination=" + encodeURIComponent(JSON.stringify(pagination)) + "&user_id=" + encodeURIComponent(JSON.stringify(userData.id));
+  const token = getSavedData("authToken");
+
+  fetchAPI(fullUrl, 'GET', token).then(data => {
+    curPage = data.curPage;
+    pageTotal = data.pageTotal;
+    nextPage = data.nextPage;
+    prevPage = data.prevPage;
+    populateToTransactionTable(data)
+    displayPaginationList(data, 'table-transaction-pagination', 'table-transaction-pagination-column', getTransactionData)
+  }).catch(error => {});
+}
+
+function populateToTransactionTable(rawData) {
+
+	  const data = rawData.items;
+    const parentTable = document.getElementById('table-transaction-list');
+    const style = document.getElementById('table-transaction-row')
+    const emptyDiv = [];
+
+    data.forEach(data => {
+        const card = style.cloneNode(true)
+        const divs = card.getElementsByTagName('div')
+        
+        const transId = divs[1]
+        transId.textContent = data.id;
+      
+        const eWalletCompanyName = divs[2]
+        eWalletCompanyName.textContent = data.ewallet_company_data.name;
+        const dateTime = divs[4]
+        dateTime.textContent = data.created_at;
+        const price = divs[6]
+        price.textContent = data.request_amount_to_be_credit;
+        /*const weight = divs[8]
+        weight.textContent = data.weight;
+        */
+        const status = divs[11]
+        status.textContent = data.status_data.type;
+     
+
+        emptyDiv.push(card)
+    })
+
+    // Remove all old child nodes 
+    while (parentTable.firstChild) {
+        parentTable.removeChild(parentTable.firstChild);
+    }
+
+    emptyDiv.forEach(item => {
+        parentTable.appendChild(item);
+    });
+}
+
+document.getElementById('table-transaction-btn-next').addEventListener('click', () => {
+   if (nextPage !== null && curPage < pageTotal) {
+     curPage++;
+     getTransactionData(curPage)
+  }
+});
+
+document.getElementById('table-transaction-btn-prev').addEventListener('click', () => {
+  if (prevPage !== null && curPage > 1) {
+     curPage--;
+     getTransactionData(curPage)
+  }
+});
+
+
+
+
 
