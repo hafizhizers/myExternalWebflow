90439f5db5297a34096c7eeb81ca4329cd94d04c
hafizhizers <34477194+hafizhizers@users.noreply.github.com>
2022-12-19 22:33:23 +0800

    Update and rename warehouse-v1.js to warehouse-v2.js

--- warehouse-v2.js
+++ warehouse-v2.js
@@ -1,10 +1,8 @@
-
 window.onload = function() {
     getWarehouseCardList()
 }
 
 function getWarehouseCardList() {
-  
     const token = getSavedData("authToken");
     fetchAPI("https://x8ki-letl-twmt.n7.xano.io/api:bQZrLIyT/warehouse", 'GET', token)
         .then(data => {
@@ -13,22 +11,48 @@ function getWarehouseCardList() {
         .catch(error => {
           
         });
-  
 }
 
         
 function populateToWarehouseCardList(data) {
-  
     const parentTable = document.getElementById('warehouse-card-list-parent');
     const style = document.getElementById('warehouse-card-list-child')
-  
   	const emptyDiv = [];
 
     data.forEach(data => {
       const card = style.cloneNode(true);
       const divs = card.getElementsByTagName('div');
-
+      const divs0parent = divs[1]
       
+      const city = divs0parent.getElementsByTagName('h5')[0]
+      city.textContent = data.city;
+      const flag = divs0parent.getElementsByTagName('img')[0]; // Assuming there is only one <img> element
+      flag.src = data.country_flag.url;
+      const postalCode = divs[8];
+      postalCode.textContent = data.postal_code;
+      const countryDisctrict = divs[12];
+      countryDisctrict.textContent = data.country_disctrict;
+      const lockerNumber = divs[16];
+      lockerNumber.textContent = data.locker_number;
+      
+      
+      const divImg = divs[2];
+      divImg.innerHTML = "";
+      
+      data.transport_by.forEach(transportation => {
+        let imgSrc;
+        if (transportation.code === 1) {
+          imgSrc = "https://uploads-ssl.webflow.com/637071c64a3e3c44d79c03cf/638b72cf77d1c720069834fb_airplane.svg";
+        } 
+        else if (transportation.code === 2) {
+          imgSrc = "https://uploads-ssl.webflow.com/637071c64a3e3c44d79c03cf/638b72d0c239de48ccb51f85_Property%201%3DLinear%2C%20Property%202%3Dtruck.svg";
+        } 
+        else if (transportation.code === 3) {
+          imgSrc = "https://uploads-ssl.webflow.com/637071c64a3e3c44d79c03cf/638b72d09bf1388130d18138_Property%201%3DLinear%2C%20Property%202%3Dship.svg";
+        } 
+        divImg.innerHTML += `<img loading="lazy" src="${imgSrc}" alt="">`;
+      });
+    
       card.addEventListener('click', function() {
 
         card.classList.add("w--current");
@@ -55,7 +79,9 @@ function populateToWarehouseCardList(data) {
         cellphoneNumber.textContent = data.cellphone_number;
         const remarks = document.getElementById('warehouse-detail-remarks')
         remarks.textContent = data.remarks;
-   
+        const transportListDetail = document.getElementById('warehouse-detail-transport-list')
+        transportListDetail.innerHTML = divImg.innerHTML;
+
         emptyDiv.forEach(otherCard => {
           if (otherCard !== card) {
           	otherCard.classList.remove("w--current");
@@ -63,19 +89,8 @@ function populateToWarehouseCardList(data) {
         });
       });
 
-			const divs0parent = divs[1]
-      const city = divs0parent.getElementsByTagName('h5')[0]
-      city.textContent = data.city;
-	    
-      const flag = divs0parent.getElementsByTagName('img')[0]; // Assuming there is only one <img> element
-      flag.src = data.country_flag.url;
-      
-      const postalCode = divs[8];
-      postalCode.textContent = data.postal_code;
-      const countryDisctrict = divs[12];
-      countryDisctrict.textContent = data.country_disctrict;
-      const lockerNumber = divs[16];
-      lockerNumber.textContent = data.locker_number;
+     
+     
       
       emptyDiv.push(card);
     });
@@ -123,5 +138,3 @@ function copyTextToClipboard(buttonId, elementId) {
   document.body.removeChild(textarea);
   alert('Text copied to clipboard');
 }
-
-
